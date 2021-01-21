ff1d32a074a2b59afa2086df9ea8e097074163e2
Yunkai Zhang (Caroline) <rorelynn@hotmail.com>
2021-01-22 00:14:35 +0800

    added DisplayRules feature

--- dicegame/game.py
+++ dicegame/game.py
@@ -11,4 +11,15 @@ def menuChoice():
 		print("Please enter a number between 1 and 3")
 		choice=input()
 	return choice
+	
+#subroutine to display rules
+
+def displayRules():
+	print("The rules of the game are as follows:")
+	print("Players take turns to throw two dice.")
+	print("If the throw is a 'double', i.e. two 2s, two 3s, etc.,")
+	print("the player's score reverts to zero and their turn ends.")
+	print("(etc.)")
+	
+
 		
