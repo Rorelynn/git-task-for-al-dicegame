6dfe374afa008d53b4412ef723ddc6934f2d84c6
Yunkai Zhang (Caroline) <rorelynn@hotmail.com>
2021-01-22 00:08:55 +0800

    MenuChoice Setted

--- dicegame/game.py
+++ dicegame/game.py
@@ -1,2 +1,14 @@
 #intro
 #this is a 2-player dice game
+def menuChoice():
+	print("Option 1: Display rules")
+	print("Option 2: Start new game")
+	print("Option 3: Quit")
+	print("What would you like to do?")
+	choice=input();
+	while (choice != 1 and choice != 2 and choice != 3):
+		print("That is not a valid choice")
+		print("Please enter a number between 1 and 3")
+		choice=input()
+	return choice
+		
